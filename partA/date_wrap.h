94dbcc988e4a89707686a82318c4d65e9f593ecb
Tamir Offen <tamiroffen@gmail.com>
2021-01-26 10:49:36 +0200

    Revert "Added comments."
    This reverts commit 94aba931a75be9597c0743936543a1a59e8c48c1.

--- partA/date_wrap.h
+++ partA/date_wrap.h
@@ -3,15 +3,12 @@
 
 #include <iostream>
 #include "date.h"
-using std::ostream;
 
 namespace mtm
 {
-    //A class which represents a date (consisting of day, month and year).
     class DateWrap;
-    //Both functions are being put here because both are declared as friend functions.
-    ostream& operator<< (ostream& out, const DateWrap& date);//print operator
-    DateWrap operator+ (int, DateWrap&);//int before date for instance: (3 + date1).
+    std::ostream& operator<< (std::ostream& out, const DateWrap& date);
+    DateWrap operator+ (int, DateWrap&);
 }
 
     class mtm::DateWrap 
@@ -21,26 +18,27 @@ namespace mtm
         int year;
 
     public:
-        DateWrap(int day, int month, int year); // Constructor
-        DateWrap(const DateWrap&);// Copy constructor
+        DateWrap(int day, int month, int year);
+        DateWrap(const DateWrap&);
+        ~DateWrap() = default;
 
-        const int dayReturn () const;//Returns day.
-        const int monthReturn () const;//returns month.
-        const int yearReturn() const;//returns year.
+        const int dayReturn () const; 
+        const int monthReturn () const;
+        const int yearReturn() const;
 
 
-        friend ostream& operator<< (ostream& out, const DateWrap& date);
-        bool operator< (const DateWrap&) const;//less than operator (this < date2)
-        bool operator> (const DateWrap&) const;//more than operator (this > date2)
-        bool operator== (const DateWrap&) const;//is equal operator (this == date2)
-        bool operator<= (const DateWrap&) const;//less than or equal operator (this <= date2)
-        bool operator>= (const DateWrap&) const;//more than or equal operator (this >= date2)
-        bool operator!= (const DateWrap&) const;//Not equal operator (this != date2)
-        DateWrap operator++ (int); //this++
-        DateWrap& operator++ () = delete; //++i not needed 
-        DateWrap& operator+= (int); // this += date2 -----> eventually returns this by reference after adding date2.
-        DateWrap operator+ (int); // this + 3 --------> eventually returns a new date which occurs 3 days after (this).
-        friend DateWrap operator+ (int, DateWrap&); // 3 + this , eventually returns a new date which occurs 3 days after (this).
+        friend std::ostream& operator<< (std::ostream& out, const DateWrap& date);
+        bool operator< (const DateWrap&) const;
+        bool operator> (const DateWrap&) const;
+        bool operator== (const DateWrap&) const;
+        bool operator<= (const DateWrap&) const;
+        bool operator>= (const DateWrap&) const;
+        bool operator!= (const DateWrap&) const;
+        DateWrap operator++ (int); //i++
+        DateWrap& operator++ () = delete; //++i (is not supported)
+        DateWrap& operator+= (int); 
+        DateWrap operator+ (int); 
+        friend DateWrap operator+ (int, DateWrap&);
         
         
     };
