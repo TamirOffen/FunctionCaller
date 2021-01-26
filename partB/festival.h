13d5c5c5aff0f564ad11fb05c50a1ccce75c8f32
Tamir Offen <tamiroffen@gmail.com>
2021-01-26 10:48:16 +0200

    Revert "Added comments"
    This reverts commit 7d414315aac6e97cb8ab40db1375b464b856b7ee.

--- partB/festival.h
+++ partB/festival.h
@@ -5,21 +5,20 @@
 
 namespace mtm 
 {
-    //A subclass which represents a festival management which occurs on a specific date.
     class Festival : public EventContainer 
     {
 
     private:
-        DateWrap date_of_festival;//Festival's date
+        DateWrap date_of_festival;
 
     public:
-        Festival(const DateWrap&);//Constructor
+        Festival(const DateWrap&);
         ~Festival() = default;
 
-        //Adds the given event to the festival only if they occur on the same date.
         void add(const BaseEvent&) override;
 
     };
 }
 
 #endif
+
