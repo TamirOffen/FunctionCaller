0e1be77f65e5568ef0454cd75801961702f1b7fa
NabeehAS <75035002+NabeehAS@users.noreply.github.com>
2021-01-26 02:07:56 +0200

    Added comments

--- partB/event_container.h
+++ partB/event_container.h
@@ -5,33 +5,36 @@
 
 namespace mtm 
 {
+    //A stuct which represents each event and knows the event which occurs after it.
     struct EventNode 
     {
         BaseEvent *event;
         EventNode *next;
     };
 
+    //A head class which represents a container of multiple events.
     class EventContainer 
     {
     private:
         EventNode *head = NULL;
 
     protected:
-        void add_event(const BaseEvent&); 
+        void add_event(const BaseEvent&); //todo delete and move to festival
 
     public:
         EventContainer();
         ~EventContainer();
-
+        //Gets a BaseEvent and adds it to the current events container if possible.
         virtual void add(const BaseEvent&) = 0;
 
+        //An events iterator.
         class EventIterator
         {
         private:
             EventNode *current_node;
 
         public:
-            // EventIterator();
+            
             EventIterator(const EventIterator&);
             EventIterator(const EventContainer&, bool is_end);
             ~EventIterator() = default;
@@ -40,13 +43,14 @@ namespace mtm
             EventIterator& operator++ (); //++i
             BaseEvent& operator* (); //*i (dereference)
 
-            // returns T/F if the iterators are pointing to the same event
-            bool operator== (const EventIterator& iter) const; // iter1 == iter2
-            bool operator!= (const EventIterator& iter) const; // iter1 != iter2
+           
+            bool operator== (const EventIterator& iter); // iter1 == iter2
+            bool operator!= (const EventIterator& iter); // iter1 != iter2
         };
 
-
+        //Returns the first event in the events container.
         EventIterator begin() const;
+        //Returns the last event in the events container.
         EventIterator end() const;
 
     };
@@ -54,5 +58,4 @@ namespace mtm
 
 
 #endif
-
 
