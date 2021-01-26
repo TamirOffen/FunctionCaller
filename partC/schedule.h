951755fb11868cf4e70b606bc152134936c320bf
Tamir Offen <tamiroffen@gmail.com>
2021-01-26 10:43:55 +0200

    Revert "Added comments"
    This reverts commit fa3844a52f9a4d5690a89db4867d0ca9381390dc.

--- partC/schedule.h
+++ partC/schedule.h
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <set>
 
-using std::set;
+// using std::set;
 
 
 namespace mtm 
@@ -15,39 +15,30 @@ namespace mtm
         bool operator() (const BaseEvent* lhs, const BaseEvent* rhs) const;
     };
 
-    //A class that represents a schedule of events, which allows adding events to it and students to the events in it.
     class Schedule
     {
 
     private:
-        //A set of events in the current schedule.
-        set<BaseEvent*, compare> events;
-        //Checks whether the given event is currently in the schedule.
+        std::set<BaseEvent*, compare> events;
         bool isInEvents(BaseEvent&) const;
-        bool canAddEventContainer(const EventContainer&);
-        bool eventContainerIsLegal(const EventContainer&); //doesn't contain dublicate events
-        BaseEvent* getBaseEvent(const DateWrap&, const string&) const; //returns NULL if event isn't in events
+        bool canAddEventContainer(const EventContainer&) const;
+        bool eventContainerIsLegal(const EventContainer&) const; //doesn't contain dublicate events
+        BaseEvent* getBaseEvent(const DateWrap&, const std::string&) const; //returns NULL if event isn't in events
 
     public:
-        Schedule() = default;//Constructor
+        Schedule() = default;
         ~Schedule();
-        
-        //Adds a container of events to the schedule.
+
         void addEvents(const EventContainer&); 
 
-        //Adds the given student (int) to the event with the same name and date as given if it finds it in schedule.
-        void registerToEvent(const DateWrap&, const string&, const int);
-        //Removes the given student (int) to the event with the same name and date as given if it finds it in schedule.
-        void unregisterFromEvent(const DateWrap&, const string&, const int);
+        // date + name => event
+        void registerToEvent(const DateWrap&, const std::string&, const int);
+        void unregisterFromEvent(const DateWrap&, const std::string&, const int);
 
-        //Prints all the events in the schedule.
         void printAllEvents() const;
-        //Prints all the events in the schedule which occur on the same month and year as given.
         void printMonthEvents(const int month, const int year) const;
-        //Prints the event with the same name and date as given in detail.
-        void printEventDetails(const string&, const DateWrap&) const;
+        void printEventDetails(const std::string&, const DateWrap&) const;
 
-        // Prints all the events which the given predicate returns true over them, detailed print if verbose it true and short print if not.
         template <class Predicate>
         void printSomeEvents(Predicate predicate, const bool verbose) const;
 
@@ -57,7 +48,7 @@ namespace mtm
 
 template <class Predicate>
 void mtm::Schedule::printSomeEvents(Predicate predicate, const bool verbose) const {
-    set<BaseEvent*>::iterator iter = events.begin();
+    std::set<BaseEvent*>::iterator iter = events.begin();
     for( ; iter != events.end(); ++iter) 
     {
         if(predicate(**iter) == true) 
@@ -80,3 +71,5 @@ void mtm::Schedule::printSomeEvents(Predicate predicate, const bool verbose) con
 
 
 #endif
+
+
