f29e369ad0c98d7568b7f4af810ebcffeb2b2bf0
Tamir Offen <tamiroffen@gmail.com>
2021-01-26 12:12:37 +0200

    problems with date_wrap

--- partA/date_wrap.cpp
+++ partA/date_wrap.cpp
@@ -3,16 +3,15 @@
 #include "exceptions.h"
 
 using mtm::DateWrap;
-using std::ostream;
 
 extern "C" 
 {
     #include "date.h"
 }
-
-DateWrap::DateWrap(int day, int month, int year) : 
-    day(day), month(month), year(year)
+//Initialize this's day month and year to the given day month and year.
+DateWrap::DateWrap(int day, int month, int year) : date_day(day), date_month(month), date_year(year)
 {
+    //In case of invalid date.
     if(day < 1 || day > 30 || month < 1 || month > 12) 
     {
         throw mtm::InvalidDate();
@@ -20,39 +19,39 @@ DateWrap::DateWrap(int day, int month, int year) :
 }
 
 
-const int DateWrap::day() const
+const int DateWrap::day () const
 {
-    return day;
+    return date_day;
 }
 
-const int DateWrap::month() const
+const int DateWrap::month () const
 {
-    return month;
+    return date_month;
 }
 
 const int DateWrap::year() const
 {
-    return year;
+    return date_year;
 }
 
-
+//Prints in the required format (d/m/yyyy)
 ostream& mtm::operator<<(ostream& out, const DateWrap& date)
 {
-    out << date.day << '/' << date.month << '/' << date.year;
+    out << date.date_day << '/' << date.date_month << '/' << date.date_year;
     return out;
 }
 
 bool DateWrap::operator< (const DateWrap& date) const
 {
-    if(this->year < date.year){
+    if(this->date_year < date.date_year){
         return true;
-    }else if(this->year > date.year){
+    }else if(this->date_year > date.date_year){
         return false;
-    }else if(this->month < date.month){
+    }else if(this->date_month < date.date_month){
         return true;
-    }else if(this->month > date.month){
+    }else if(this->date_month > date.date_month){
         return false;
-    }else if (this->day < date.day){
+    }else if (this->date_day < date.date_day){
         return true;
     }else {
         return false;
@@ -61,12 +60,13 @@ bool DateWrap::operator< (const DateWrap& date) const
 
  bool DateWrap::operator> (const DateWrap& date) const
  {
+     //Uses the less than operator function.
      return (date < *this);
  }
 
 bool DateWrap::operator== (const DateWrap& date) const
 {
-    if (this->day == date.day && this->month == date.month && this->year == date.year){
+    if (this->date_day == date.date_day && this->date_month == date.date_month && this->date_year == date.date_year){
         return true;
     }
     return false;  
@@ -74,25 +74,33 @@ bool DateWrap::operator== (const DateWrap& date) const
 
 bool DateWrap::operator<= (const DateWrap& date) const
 {
+    //Uses the is equal operator function.
     return (*this < date || *this == date);
 }
 
 bool DateWrap::operator>= (const DateWrap& date) const
 {
+    ////Uses the less than or equal operator function.
     return (date <= *this);
 }
 
 bool DateWrap::operator!= (const DateWrap& date) const
 {
+    //Uses the is equal operator function.
     return (!(*this == date));
 }
 
 DateWrap DateWrap::operator++ (int)
 {
+    //Creates a new DateWrap to store this in it.
     DateWrap temp = *this;
-    Date date = dateCreate(this->day, this->month, this->year);
+    //Creates a new date and initializes it with copeis of the values of the current DateWrap (this).
+    Date date = dateCreate(this->date_day, this->date_month, this->date_year);
+    //Increases the date by 1 day.
     dateTick(date);
-    dateGet(date, &this->day, &this->month, &this->year);
+    //put the new values of date after the increment into the day month and year of the current date (this).
+    dateGet(date, &this->date_day, &this->date_month, &this->date_year);
+    //Frees the created date.
     dateDestroy(date);
     return temp;    
 }
@@ -103,50 +111,50 @@ DateWrap& DateWrap::operator+= (int increment)
         throw mtm::NegativeDays();
     }
 
-    Date date = dateCreate(this->day, this->month, this->year);
+    //Creates a new date and puts the current's date information copies in it.
+    Date date = dateCreate(this->date_day, this->date_month, this->date_year);
+    //Increases the date by (increment) days.
     for (int i = 0 ; i < increment ; i++)
     {
         dateTick(date);
     }
-    dateGet(date, &this->day, &this->month, &this->year);
-
+    //Put the day month and year of date after the addition into the current date (this).
+    dateGet(date, &this->date_day, &this->date_month, &this->date_year);
+    //Frees the created date.
     dateDestroy(date);
     return *this;
 }
 
-DateWrap DateWrap::operator+(int increment)
+DateWrap DateWrap::operator+(int increment)// this + 3
 {
     if(increment < 0)
     {
         throw mtm::NegativeDays();
     }
-
-    DateWrap newDate(this->day, this->month, this->year);
+    //Uses the += operator
+    DateWrap newDate(this->date_day, this->date_month, this->date_year);
     newDate += increment;
 
     return newDate;
 }
 
-DateWrap mtm::operator+ (int increment, DateWrap& date)
+DateWrap mtm::operator+ (int increment, DateWrap& date)// 3 + this
 {
     if(increment < 0)
     {
         throw mtm::NegativeDays();
     }
-
-    DateWrap newDate(date.day, date.month, date.year);
+    //Uses the += operator
+    DateWrap newDate(date.date_day, date.date_month, date.date_year);
     newDate += increment;
 
     return newDate;
 }
 
 
-
+//copy constructor
 DateWrap::DateWrap(const DateWrap& date) :
-    day(date.day), month(date.month), year(date.year)
+    date_day(date.date_day), date_month(date.date_month), date_year(date.date_year)
 {
 }
-
-
-
 
--- partA/date_wrap.h
+++ partA/date_wrap.h
@@ -3,42 +3,44 @@
 
 #include <iostream>
 #include "date.h"
+using std::ostream;
 
 namespace mtm
 {
+    //A class which represents a date (consisting of day, month and year).
     class DateWrap;
-    std::ostream& operator<< (std::ostream& out, const DateWrap& date);
-    DateWrap operator+ (int, DateWrap&);
+    //Both functions are being put here because both are declared as friend functions.
+    ostream& operator<< (ostream& out, const DateWrap& date);//print operator
+    DateWrap operator+ (int, DateWrap&);//int before date for instance: (3 + date1).
 }
 
     class mtm::DateWrap 
     {
-        int day;
-        int month;
-        int year;
+        int date_day;
+        int date_month;
+        int date_year;
 
     public:
-        DateWrap(int day, int month, int year);
-        DateWrap(const DateWrap&);
-        ~DateWrap() = default;
+        DateWrap(int day, int month, int year); // Constructor
+        DateWrap(const DateWrap&);// Copy constructor
 
-        const int day() const; 
-        const int month() const;
-        const int year() const;
+        const int day () const;//Returns day.
+        const int month () const;//returns month.
+        const int year() const;//returns year.
 
 
-        friend std::ostream& operator<< (std::ostream& out, const DateWrap& date);
-        bool operator< (const DateWrap&) const;
-        bool operator> (const DateWrap&) const;
-        bool operator== (const DateWrap&) const;
-        bool operator<= (const DateWrap&) const;
-        bool operator>= (const DateWrap&) const;
-        bool operator!= (const DateWrap&) const;
-        DateWrap operator++ (int); //i++
-        DateWrap& operator++ () = delete; //++i (is not supported)
-        DateWrap& operator+= (int); 
-        DateWrap operator+ (int); 
-        friend DateWrap operator+ (int, DateWrap&);
+        friend ostream& operator<< (ostream& out, const DateWrap& date);
+        bool operator< (const DateWrap&) const;//less than operator (this < date2)
+        bool operator> (const DateWrap&) const;//more than operator (this > date2)
+        bool operator== (const DateWrap&) const;//is equal operator (this == date2)
+        bool operator<= (const DateWrap&) const;//less than or equal operator (this <= date2)
+        bool operator>= (const DateWrap&) const;//more than or equal operator (this >= date2)
+        bool operator!= (const DateWrap&) const;//Not equal operator (this != date2)
+        DateWrap operator++ (int); //this++
+        DateWrap& operator++ () = delete; //++i not needed 
+        DateWrap& operator+= (int); // this += date2 -----> eventually returns this by reference after adding date2.
+        DateWrap operator+ (int); // this + 3 --------> eventually returns a new date which occurs 3 days after (this).
+        friend DateWrap operator+ (int, DateWrap&); // 3 + this , eventually returns a new date which occurs 3 days after (this).
         
         
     };
